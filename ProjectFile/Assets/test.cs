9cc47065e63d0d6c74263dcbbd807fc8ab8d2b35
Goktug Geneci <goktuggeneci@gmail.com>
2019-09-12 10:09:15 -0400

    Initial project folder

---
+++ ProjectFile/Assets/test.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Goktug test
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
