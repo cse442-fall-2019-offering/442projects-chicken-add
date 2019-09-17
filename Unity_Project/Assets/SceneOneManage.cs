4d309283e4e4225956b385562ec64aa119967436
Tom DePaola <tjdepaol@buffalo.edu>
2019-09-17 12:23:56 -0400

    Added Main Menu, Options Menu, Some funtionality
    There is now a main menu with clickable buttons that each have a function. Quit ends the game, Options brings up an options menu and play enters the next scene. The back button on the game screen has no functionality yet.

---
+++ Unity_Project/Assets/SceneOneManage.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneOneManage : MonoBehaviour
+{
+    int sceneIndex; 
+    //used to track which scene we are on
+    public void Start()
+    {
+        sceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+    public void Back()
+    {
+        if (sceneIndex > 0)
+        {
+            SceneManager.LoadScene(sceneIndex - 1);
+           
+        }
+        else
+        {
+            Application.Quit();
+        }
+    }
+}
