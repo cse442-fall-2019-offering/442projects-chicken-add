ab0bbdb43fdf5781d52cceb42c9513e540f1698e
Michael Mincone <michaelmincone@gmail.com>
2019-09-25 18:13:54 -0400

    added a basic UI in leaderboard
    added a title and row for username and score

---
+++ Unity_Project/Assets/HighScore.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private Transform entryContainer;
+    private Transform entryTemplate;
+    // Start is called before the first frame update
+    void Start(){
+         entryContainer = transform.Find("scoringContainer");
+        entryTemplate = transform.Find("scoringTemplate");
+        entryTemplate.gameObject.SetActive(false);
+        
+        float templateHeight = 30f;
+        for(int i = 0; i < 10; ++i){
+            Transform entryTransform = Instantiate(entryTemplate, entryContainer);
+            RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
+            entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * i);
+            entryTransform.gameObject.SetActive(true);
+        }
+    }
+    
+
+    // Update is called once per frame
+    void Update(){
+       
+    }  
+}
