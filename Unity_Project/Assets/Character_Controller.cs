c5d4ebb4923f257d1986816657346c70c92e4ada
Tom DePaola <tjdepaol@buffalo.edu>
2019-10-22 02:08:15 -0400

    Fix Merge Issues #53
    Branches did not merge correctly so I needed to debug the game and make fixes to many aspects.

--- Unity_Project/Assets/Character_Controller.cs
+++ Unity_Project/Assets/Character_Controller.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Character_Controller : MonoBehaviour
 {
@@ -9,25 +10,30 @@ public class Character_Controller : MonoBehaviour
     private float moveInput;
 
     private Rigidbody2D rb;
-
+    private BoxCollider2D box;
     private bool facingRight = true;
 
     private bool isGrounded;
     public Transform groundCheck;
+    private bool isCrouching = false;
+    private bool standUp = false;
+    public Transform crouchCheck;
+    private bool crouch;
     public float checkRadius;
     public LayerMask whatIsGround;
-
+    public Animator animator;
 
 
     void Start()
     {
+        box = GetComponent<BoxCollider2D>();
         rb = GetComponent<Rigidbody2D>();
     }
 
     void FixedUpdate()
     {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
-
+        crouch = Physics2D.OverlapCircle(crouchCheck.position, checkRadius, whatIsGround);
         moveInput = Input.GetAxis("Horizontal");
         //right arrow input = 1, left = -1
         rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
@@ -38,13 +44,62 @@ public class Character_Controller : MonoBehaviour
         {
             Flip();
         }
+        animator.SetFloat("Speed", Math.Abs(moveInput * speed));
     }
     private void Update()
     {
+        //Debug.Log(isCrouching);
+        //Debug.Log(crouch);
+      if ((Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) && isCrouching == false)
+        {
+            box.enabled = false;
+            isCrouching = true;
+            //rb.velocity = new Vector2((moveInput * speed) / 2, rb.velocity.y);
+        }
+        else if ((Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.S)) && (crouch == false))
+        {
+            box.enabled = true;
+            isCrouching = false;
+        }
+        if((Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.S)) && (crouch == true))
+        {
+            standUp = true;
+        }
+        if (standUp && (crouch == false))
+        {
+            box.enabled = true;
+            standUp = false;
+            isCrouching = false;
+        }
+        /*if ((Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.S)) && (crouch == false))
+        {
+            box.enabled = true;
+            isCrouching = false;
+        }*/
+
+
+
         if ((Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) && isGrounded == true)
         {
+           
             rb.velocity = Vector2.up * jumpForce;
+            animator.SetBool("IsJumping", true);
+            //animator.SetFloat("Height", groundCheck.position.y);
+            
         }
+      if ((!Input.GetKeyDown(KeyCode.UpArrow) && !Input.GetKeyDown(KeyCode.W)) && isGrounded == false)
+      {
+        animator.SetBool("IsJumping", true);
+      }
+      if ((!Input.GetKeyDown(KeyCode.UpArrow) && !Input.GetKeyDown(KeyCode.W)) && isGrounded == true) {
+        animator.SetBool("IsJumping", false);
+      }
+
+
+
+      
+        
+       
     }
     //This function flips the orientation of the sprite, using this
     //before animation.
@@ -57,4 +112,5 @@ public class Character_Controller : MonoBehaviour
 
         //comment boi
     }
-}
+
+   }
