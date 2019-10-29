68852fff0a9f8252926bb76b056f81058eeb4b5b
Brian <brbrown2@buffalo.edu>
2019-10-29 12:10:14 -0400

    Coded Levels UI #2
    Coded the Levels UI to properly display the current level and reset when game over is triggered or when current game is exited.

--- Unity_Project/Assets/GameMaster.cs
+++ Unity_Project/Assets/GameMaster.cs
@@ -7,19 +7,25 @@ using UnityEngine.SceneManagement;
 public class GameMaster : MonoBehaviour
 {
     public static GameMaster gm;
+    //Lives UI var
     public static int lives;
+    //Level UI var
+    public static int levels;
+    //Lives displays
     public GameObject Hertz1, Hertz2, Hertz3;
     //public static GameObject SpawnPoint;
     /*
     public static CinemachineVirtualCamera myCinemachine;
     double nextTimeToSeach = 0;
     */
+    //On start, display the lives and initialize the variables to the global variables in LivesControl
     void Start()
     {
         Hertz1.SetActive(true);
         Hertz2.SetActive(true);
         Hertz3.SetActive(true);
         lives = LivesControl.Instance.life;
+        levels = LivesControl.Instance.level;
         if (gm == null)
         {
             gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
@@ -27,15 +33,19 @@ public class GameMaster : MonoBehaviour
         //myCinemachine = GetComponent<CinemachineVirtualCamera>();
     }
 
+    //respawn values
     public Transform playerPrefab;
     public Transform spawnPoint;
     public int spawnDelay = 1;
 
+    //saves local level and lives variables to the global variables in LivesControl
     public void SavePlayer()
     {
         LivesControl.Instance.life = lives;
+        LivesControl.Instance.level = levels;
     }
     
+    //Respawns the player
     public void RespawnPlayer()
     {
         //yield return new WaitForSeconds(spawnDelay);
@@ -62,11 +72,14 @@ public class GameMaster : MonoBehaviour
             */
     }
 
+    //constantly checks how many lives the  player has left.
     private void Update()
     {
+        //if player has no more lives, values are reset, the current instance is destroyed and the gameover scene is loaded.
         if (lives == 0)
         {
             lives = 3;
+            levels = 1;
             gm.SavePlayer();
             Destroy(this.gameObject);
             SceneManager.LoadScene("GameOver");
@@ -74,12 +87,14 @@ public class GameMaster : MonoBehaviour
         }
         else
         {
+            //if the player has two lives, only one life display gets disabled.
             if (lives == 2)
             {
                 Hertz1.SetActive(false);
             }
             else
             {
+                //if the player has one life, two life displays are disabled.
                 if (lives == 1)
                 {
                     Hertz1.SetActive(false);
@@ -89,12 +104,8 @@ public class GameMaster : MonoBehaviour
             gm.SavePlayer();
         }
     }
-
-    public GameObject getThis()
-    {
-        return this.gameObject;
-    }
     
+    //kills player and decrements life. Player object is destroyed and respawned at the spawnpoint.
     public static void KillPlayer(Player player)
     {
         Debug.Log("Player Destroyed");
--- Unity_Project/Assets/LivesControl.cs
+++ Unity_Project/Assets/LivesControl.cs
@@ -6,8 +6,11 @@ using UnityEngine.SceneManagement;
 public class LivesControl : MonoBehaviour
 {
     public static LivesControl Instance;
+    //Global lives and levels variables
     public int life = 3;
+    public int level = 1;
 
+    //checks if instance is already running, if not creates a "permanent" game object.
     void Awake()
     {
         if (Instance == null)
