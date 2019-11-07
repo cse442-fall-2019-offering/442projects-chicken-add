8e74954c64c0d7fba599df776a13b083df68b94b
Tom DePaola <tjdepaol@buffalo.edu>
2019-11-07 13:20:26 -0500

    Added scene for code puzzle #73
    Code puzzle scene up, need to do drag and drop of words.

---
+++ Unity_Project/Assets/DragDrop.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragDrop : MonoBehaviour
+{
+    public GameObject python, python_blank;
+
+    Vector2 pythonInitPos;
+
+    void Start()
+    {
+        pythonInitPos = python.transform.position;
+    }
+
+    void Update()
+    {
+     if (Input.GetKeyDown(KeyCode.Mouse0))
+     {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    pythonInitPos.x = Input.mousePosition.x;
+                    pythonInitPos.y = Input.mousePosition.y;
+                    transform.position = pythonInitPos;
+                }
+            }
+
+        }
+    }
+
