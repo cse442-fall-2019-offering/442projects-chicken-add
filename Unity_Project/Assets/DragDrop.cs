8282876df76ec3f815d80d4f324552b76f3bdd7e
Tom DePaola <tjdepaol@buffalo.edu>
2019-11-07 14:37:13 -0500

    Added Drag Functionality #74
    Words can now drag and return to their original position

--- Unity_Project/Assets/DragDrop.cs
+++ Unity_Project/Assets/DragDrop.cs
@@ -1,30 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class DragDrop : MonoBehaviour
+public class DragDrop : MonoBehaviour, IDragHandler, IEndDragHandler
 {
-    public GameObject python, python_blank;
+    public GameObject word, word_blank;
 
-    Vector2 pythonInitPos;
+    Vector2 wordInitPos;
+    Vector3 blankPos;
+    
 
     void Start()
     {
-        pythonInitPos = python.transform.position;
+        wordInitPos = word.transform.position;
+        blankPos = word_blank.transform.position;
+        //word2InitPos = word2.transform.position;
+        //word3InitPos = word3.transform.position;
+        //word4InitPos = word4.transform.position;
     }
 
     void Update()
     {
-     if (Input.GetKeyDown(KeyCode.Mouse0))
-     {
-                if (Input.GetMouseButtonDown(0))
-                {
-                    pythonInitPos.x = Input.mousePosition.x;
-                    pythonInitPos.y = Input.mousePosition.y;
-                    transform.position = pythonInitPos;
-                }
-            }
+        /*if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+                   if (Input.GetMouseButtonDown(0))
+                   {
+                       pythonInitPos.x = Input.mousePosition.x;
+                       pythonInitPos.y = Input.mousePosition.y;
+                       transform.position = pythonInitPos;
+                   }
+               }
 
-        }
+           */
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
     }
 
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if(transform.position == blankPos)
+        {
+            Debug.Log("Right Position");
+            transform.position = Input.mousePosition;
+        }
+        else
+        {
+            transform.position = wordInitPos;
+        }
+        
+    }
+}
